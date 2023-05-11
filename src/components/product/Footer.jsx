9db3b818a6b291021e2d5a0a0a33e90ed7bf8788
Arley Hurtado <aguapanela242@gmail.com>
2023-05-11 12:49:48 -0500

    Version Dos

--- src/components/product/Footer.jsx
+++ src/components/product/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-black py-2 px-4 hover:bg-orange-600">
       <div className="flex justify-center">
-        <a href="https://www.linkedin.com/in/brandoll-guzman-872137266/" target="_blank" rel="noreferrer">
+        <a href="https://www.linkedin.com/in/jostin-arley-hurtado-hinestroza-57ab49255/" target="_blank" rel="noreferrer">
           <FaLinkedin className="text-white mx-2 hover:text-gray-500 transition duration-300 transform hover:scale-110 focus:scale-110 mr-6 mt-6" size={35} />
         </a>
         <a href="https://github.com/QITO17" target="_blank" rel="noreferrer">
